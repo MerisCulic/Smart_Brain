3e369bba97fd9446273c2d3fbc550ac237acaf6f
Meris Čulić <meris.culic@gmail.com>
2021-03-06 21:30:12 +0100

    Update heroku connections

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ class App extends Component {
       const image = document.getElementById('inputImage');
       const width = Number(image.width);
       const height = Number(image.height);
-      console.log(width, height)
       return {
         leftCol: clarifaiFace.left_col * width,
         topRow: clarifaiFace.top_row * height,
@@ -65,7 +64,7 @@ class App extends Component {
 
     onButtonSubmit = () => {
       this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3001/imageurl', {
+      fetch('https://serene-forest-09712.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type':'application/json'},
         body: JSON.stringify({
@@ -75,7 +74,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3001/image', {
+          fetch('https://serene-forest-09712.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify({
